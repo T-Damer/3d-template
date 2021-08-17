204b4e5381c714741872181e635fc55899f7e636
T-Damer <dpancov64@gmail.com>
2021-08-17 10:01:08 +0300

    ready to work

---
+++ README.md
@@ -0,0 +1,15 @@
+# 3D template
+
+Built with _three.js_ and _Vite_
+
+---
+
+## Steps to install
+
+1. `git clone https://github.com/T-Damer/3d-template.git`
+2. `cd 3d-template`
+3.
+
+---
+
+<a href="https://www.buymeacoffee.com/tdamer"><img src="https://img.buymeacoffee.com/button-api/?text=Support me with a coffee&emoji=☕️&slug=tdamer&button_colour=ffcc33&font_colour=000&font_family=Lato&outline_colour=000&coffee_colour=000"></a>
--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 import './style.css'
+
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#background'),
+})
+
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+camera.position.setZ(30)
+
+renderer.render(scene, camera)
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff6347,
+  wireframe: true,
+})
+const torus = new THREE.Mesh(geometry, material) // Mesh
+
+scene.add(torus)
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+  torus.rotation.z += 0.01
+
+  renderer.render(scene, camera)
+}
+
+animate()
