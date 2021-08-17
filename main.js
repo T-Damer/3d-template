2cb78dfca45313259b101bf6ae966d616f459c0b
T-Damer <dpancov64@gmail.com>
2021-08-17 10:17:42 +0300

    added light and helpers

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
@@ -22,13 +23,26 @@ camera.position.setZ(30)
 renderer.render(scene, camera)
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshStandardMaterial({
   color: 0xff6347,
-  wireframe: true,
 })
 const torus = new THREE.Mesh(geometry, material) // Mesh
 
 scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20, 20, 20)
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(
+  camera,
+  renderer.domElement
+)
 
 function animate() {
   requestAnimationFrame(animate)
@@ -36,6 +50,8 @@ function animate() {
   torus.rotation.x += 0.01
   torus.rotation.y += 0.005
   torus.rotation.z += 0.01
+
+  controls.update()
 
   renderer.render(scene, camera)
 }
